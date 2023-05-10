4b1981fdb30ffb7228e498718c717cc48eabdc14
camdenkutz <112977523+camdenkutz@users.noreply.github.com>
2023-05-10 14:22:00 -0500

    Add files via upload

---
+++ CIS180_Final/code.js
@@ -0,0 +1,4 @@
+document.getElementById("p1").style.setProperty('display','block');
+document.getElementById("p1").style.setProperty('background-color', 'navy');
+document.getElementById("p2").style.setProperty('display','block');
+document.getElementById("p2").style.setProperty('background-color', 'navy');
